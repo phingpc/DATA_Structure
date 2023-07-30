34232d88f0ddb80376d3918c1d3b7ac9fa04d0f4
PHinGJakkaphat <136901286+phingpc@users.noreply.github.com>
2023-07-30 21:12:25 +0700

    Add files via upload

---
+++ assignment2 by teacher/build/classes/javaapplication24/NewJFrame.rs
@@ -0,0 +1,8 @@
+javaapplication24.NewJFrame$5
+javaapplication24.NewJFrame$6
+javaapplication24.NewJFrame$3
+javaapplication24.Traveler
+javaapplication24.NewJFrame$4
+javaapplication24.NewJFrame$1
+javaapplication24.NewJFrame$2
+javaapplication24.NewJFrame
